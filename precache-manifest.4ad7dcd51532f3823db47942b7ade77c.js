67d43d0b62c56be8e318190354f8a6f2bb0a17bc
Ian Liao <javaok1987@gmail.com>
2020-03-23 17:18:14 +0800

    Updates

---
+++ precache-manifest.4ad7dcd51532f3823db47942b7ade77c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95c61c67e40a2b56aa8aa72081f1ec06",
+    "url": "/mask-map/index.html"
+  },
+  {
+    "revision": "601c837ed0af2f6fd892",
+    "url": "/mask-map/static/css/2.8b285f7d.chunk.css"
+  },
+  {
+    "revision": "acdcbbc226d2e71d074f",
+    "url": "/mask-map/static/css/main.2055244b.chunk.css"
+  },
+  {
+    "revision": "601c837ed0af2f6fd892",
+    "url": "/mask-map/static/js/2.96a88066.chunk.js"
+  },
+  {
+    "revision": "adbe8770dfe80185b1d2467350068428",
+    "url": "/mask-map/static/js/2.96a88066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acdcbbc226d2e71d074f",
+    "url": "/mask-map/static/js/main.5cc16675.chunk.js"
+  },
+  {
+    "revision": "e102b1445583f8bd620b",
+    "url": "/mask-map/static/js/runtime-main.80cace50.js"
+  }
+]);
